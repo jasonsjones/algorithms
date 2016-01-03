852c7854087044ff7d58d41a31c453d985727456
Jason S. Jones <jsjones96@gmail.com>
2016-01-03 07:17:07 -0800

    Implement mergesort and a few basic unit tests

--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -1,9 +1,51 @@
 (function() {
     'use strict';
 
-    function mergesort(a) {
-        return a;
+    function compareFnAscend(a, b) {
+        return a - b;
     }
 
-    module.exports = mergesort;
+    function mergeSort(arr, cmp) {
+        cmp = cmp || compareFnAscend;
+
+        var n = arr.length;
+        if (n < 2) {
+            return arr;
+        }
+        var mid = Math.floor(n / 2);
+        var left = arr.slice(0, mid);
+        var right = arr.slice(mid, n);
+        mergeSort(left, cmp);
+        mergeSort(right, cmp);
+        return merge(left, right, arr, cmp);
+    }
+
+    function merge(left, right, arr, cmp) {
+        var nLeft = left.length;
+        var nRight = right.length;
+        var i = 0;
+        var j = 0;
+        var k = 0;
+
+        while (i < nLeft && j < nRight) {
+            if (cmp(left[i], right[j]) <= 0) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while (i < nLeft) {
+            arr[k++] = left[i++];
+        }
+
+        while (j < nRight) {
+            arr[k++] = right[j++];
+        }
+
+        return arr;
+    }
+
+    module.exports = mergeSort;
+
 }());
--- sorting/test/mergesort.spec.js
+++ sorting/test/mergesort.spec.js
@@ -1,9 +1,24 @@
 var expect = require('chai').expect;
 var mergesort = require('../mergesort');
 
+var desc = function (a, b) {
+    return b - a;
+};
+
 describe('merge sort sorting algorithm', function () {
 
     it('works on empty arrays', function () {
         expect(mergesort([])).to.be.eql([]);
     });
+
+    it('sorts array of integers in ascending order', function () {
+        expect(mergesort([4, 5, 2, 1, 7, 8, 3, 6])).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8]);
+        expect(mergesort([34, 56, 77, 23, 21, 5])).to.be.eql([5, 21, 23, 34, 56, 77]);
+    });
+
+    it('sorts array of integers in descending order', function () {
+        expect(mergesort([4, 5, 2, 1, 3, 6], desc)).to.be.eql([6, 5, 4, 3, 2, 1]);
+        expect(mergesort([34, 56, 77, 23, 21, 5], desc)).to.be.eql([77, 56, 34, 23, 21, 5]);
+    });
+
 });
