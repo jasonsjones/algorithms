4a135d7d53ef02fa188aa80f59953d56e5b624c0
Jason S. Jones <jsjones96@gmail.com>
2016-01-22 21:38:55 -0800

    Add unit test for complex objs with custom comparator fn

--- sorting/test/mergesort.spec.js
+++ sorting/test/mergesort.spec.js
@@ -5,6 +5,17 @@ var desc = function (a, b) {
     return b - a;
 };
 
+var sortPeople = function (a, b) {
+    return a.age - b.age;
+};
+
+var people = [
+    {name: 'alice', age: 34},
+    {name: 'bob', age: 42},
+    {name: 'charlie', age: 32},
+    {name: 'david', age: 25}
+];
+
 describe('merge sort sorting algorithm', function () {
 
     it('works on empty arrays', function () {
@@ -19,6 +30,11 @@ describe('merge sort sorting algorithm', function () {
     it('sorts array of integers in descending order', function () {
         expect(mergesort([4, 5, 2, 1, 3, 6], desc)).to.be.eql([6, 5, 4, 3, 2, 1]);
         expect(mergesort([34, 56, 77, 23, 21, 5], desc)).to.be.eql([77, 56, 34, 23, 21, 5]);
+    });
+
+    it('sorts array of complex objects', function () {
+        expect(mergesort(people, sortPeople)[0]).to.be.eql({name: 'david', age: 25});
+        expect(mergesort(people, sortPeople)[3]).to.be.eql({name: 'bob', age: 42});
     });
 
 });
