11a1a06f2e57fe9e1c2a94366965b16d70ce742c
Jason S. Jones <jsjones96@gmail.com>
2016-02-12 08:45:00 -0800

    Add simple test

--- sorting/test/quicksort.spec.js
+++ sorting/test/quicksort.spec.js
@@ -8,4 +8,10 @@ describe('quicksort sorting algorithm', function () {
         quicksort(arr, 0, 0);
         expect(arr).to.be.eql([]);
     });
+
+    it('sorts array of integers in ascending order', function () {
+        var arr1 = [4, 5, 2, 1, 7, 8, 3, 6];
+        quicksort(arr1, 0, arr1.length - 1);
+        expect(arr1).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8]);
+    });
 });
