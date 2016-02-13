53f53a8d9e44e2f35e84076a2fdd1a14a6331d5c
Jason S. Jones <jsjones96@gmail.com>
2016-02-13 07:06:29 -0800

    Add comparator fn and simplify API

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -1,18 +1,23 @@
 (function() {
     'use strict';
 
+    // Default comparator function.  Will sort elements in ascending order.
+    function compareFnAscend(a, b) {
+        return a - b;
+    }
+
     function swap(arr, i, j) {
         var temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    function partition(arr, start, end) {
+    function partition(arr, start, end, cmp) {
         var pivot = arr[end];
         var partitionIndex = start;
 
         for (var i = start; i < end; i++) {
-            if (arr[i] <= pivot) {
+            if (cmp(arr[i], pivot) <= 0) {
                 swap(arr, i, partitionIndex);
                 partitionIndex++;
             }
@@ -21,23 +26,27 @@
         return partitionIndex;
     }
 
-    function randomizePartion(arr, start, end) {
+    function randomizePartion(arr, start, end, cmp) {
         var pivotIdx = getRandomBetween(start, end);
         swap(arr, pivotIdx, end);
-        return partition(arr, start, end);
+        return partition(arr, start, end, cmp);
     }
 
     function getRandomBetween(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    function quicksort(arr, start, end) {
-
+    function doQuicksort(arr, start, end, cmp) {
+        cmp = cmp || compareFnAscend;
         if (start < end) {
-            var partionIndex = randomizePartion(arr, start, end);
-            quicksort(arr, start, partionIndex - 1);
-            quicksort(arr, partionIndex + 1, end);
+            var partionIndex = randomizePartion(arr, start, end, cmp);
+            doQuicksort(arr, start, partionIndex - 1, cmp);
+            doQuicksort(arr, partionIndex + 1, end, cmp);
         }
+    }
+
+    function quicksort(arr) {
+        doQuicksort(arr, 0, arr.length - 1);
     }
 
     module.exports = quicksort;
--- sorting/test/quicksort.spec.js
+++ sorting/test/quicksort.spec.js
@@ -5,13 +5,13 @@ describe('quicksort sorting algorithm', function () {
 
     it('works on empty arrays', function () {
         var arr = [];
-        quicksort(arr, 0, 0);
+        quicksort(arr);
         expect(arr).to.be.eql([]);
     });
 
     it('sorts array of integers in ascending order', function () {
         var arr1 = [4, 5, 2, 1, 7, 8, 3, 6];
-        quicksort(arr1, 0, arr1.length - 1);
+        quicksort(arr1);
         expect(arr1).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8]);
     });
 });
